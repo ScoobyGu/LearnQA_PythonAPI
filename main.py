11d4a93556bb07d87aeed6422100c72108dd7b24
ScoobyGu <125991922+ScoobyGu@users.noreply.github.com>
2023-04-07 11:22:52 +0300

    Update main.py
    Выполнила задание 6

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import requests
-url = "https://playground.learnqa.ru/api/long_redirect"
+
+url = "https://playground.learnqa.ru/ajax/api/compare_query_type"
+
+# 1. Запрос без параметра method
 response = requests.get(url)
+print(response.text) # Выведет "Wrong method provided"
 
-redirect_count = len(response.history)
-final_url = response.url
+# 2. Запрос с неправильным типом
+response = requests.request("HEAD", url)
+print(response.text) # Выведет "Wrong method provided"
 
-print("Количество редиректов: ", redirect_count)
-print("Итоговый URL: ", final_url)
+# 3. Запрос с правильным значением method
+payload = {"method": "GET"}
+response = requests.get(url, params=payload)
+print(response.text) # Выведет "success"
 
+# 4. Проверка всех сочетаний
+methods = ["GET", "POST", "PUT", "DELETE"]
+for method in methods:
+    for param_method in methods:
+        if method != param_method:
+            payload = {"method": param_method}
+            if method == "GET":
+                response = requests.get(url, params=payload)
+            else:
+                response = requests.post(url, data=payload)
+            if response.text == "success":
+                print(f"Method {method} with param method {param_method} is not detected")
+        else:
+            payload = {"method": param_method}
+            if method == "GET":
+                response = requests.get(url, params=payload)
+            else:
+                response = requests.post(url, data=payload)
+            if response.text != "success":
+                print(f"Method {method} with param method {param_method} is not detected")
