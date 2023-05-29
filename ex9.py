08065c9f7f3a779245d52d2f92fbb2766eab9622
ScoobyGu <125991922+ScoobyGu@users.noreply.github.com>
2023-05-29 15:58:01 +0300

    Create ex9.py
    Задание 9

---
+++ ex9.py
@@ -0,0 +1,20 @@
+import requests
+
+login = "super_admin"
+passwords = ["123456", "password", "123456789", "12345678", "12345", "1234567", "1234567890", "qwerty", "abc123", "111111", "123123", "admin", "welcome", "monkey", "login", "letmein", "dragon", "passw0rd", "football", "master", "hello", "freedom", "whatever", "qazwsx", "trustno1"]
+
+for password in passwords:
+    response1 = requests.post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework", data={"login": "super_admin", "password": passwords})
+    cookie_value = response1.cookies.get('auth_cookie')
+    cookies = {'auth_cookie': cookie_value}
+    #print(cookies)
+
+    response2 = requests.post("https://playground.learnqa.ru/ajax/api/check_auth_cookie", cookies=cookies)
+
+    if response2.text == "You are authorized":
+        print("Пароль найден! Это", password)
+
+
+
+
+
